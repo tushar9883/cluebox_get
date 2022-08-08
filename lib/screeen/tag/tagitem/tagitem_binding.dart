13e4b21c89e8040c018acb2e6a7bd861d7bb4020
tushar9883 <hustlers.tushar@gmail.com>
2022-08-08 10:03:32 +0530

    fix in add item screen

--- lib/screeen/tag/tagitem/tagitem_binding.dart
+++ lib/screeen/tag/tagitem/tagitem_binding.dart
@@ -3,10 +3,8 @@ import 'package:clue_get/model/tag_model.dart';
 import 'package:clue_get/model/user_model.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
 import '../../../db/db_helper.dart';
 import '../../../model/additem_model.dart';
-import '../../../router/router_name.dart';
 import '../../home/home_binding.dart';
 import '../tag/tag_binding.dart';
 
@@ -36,7 +34,6 @@ class TagItemController extends BaseController {
   Future<void> getAllItemsFromTag() async {
     isLoading = true;
     tagModel = TagModel.fromDb(Get.arguments);
-    print("<><><>>>>>TagID><><>><><><>${tagModel?.uid}");
     var allItems = await DbHelp().getItemsFromTag(tagModel?.uid ?? '');
     itemList = allItems;
     isLoading = false;
@@ -60,7 +57,6 @@ class TagItemController extends BaseController {
       //To update item count of this user
       userData.first.itemCount = (userData.first.itemCount ?? 0) - 1;
       await DbHelp().udpateUserData(userData.first, userData.first.uid);
-      print("User itemCount updated___________");
 
       TagModel? tagData = await DbHelp().getTagData('${tagModel?.uid}');
       var tagIds = [];
@@ -68,7 +64,6 @@ class TagItemController extends BaseController {
         tagIds.add(tagData.uid);
         tagData.tagItemCount = (tagData.tagItemCount ?? 0) - 1;
         await DbHelp().addtag(tagData);
-        print("Removed____tagItemCount_________");
       }
 
       // Get.until((route) => Get.currentRoute == RouterName.tag);
